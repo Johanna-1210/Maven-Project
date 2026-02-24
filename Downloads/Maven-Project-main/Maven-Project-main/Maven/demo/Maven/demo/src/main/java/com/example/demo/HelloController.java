108e9adfc714b97148d269773270c93c2067a067
Johanna Fernando <johannafernando.27csa@licet.ac.in>
2026-02-24 14:20:34 +0530

    Update sussessful

--- Downloads/Maven-Project-main/Maven-Project-main/Maven/demo/Maven/demo/src/main/java/com/example/demo/HelloController.java
+++ Downloads/Maven-Project-main/Maven-Project-main/Maven/demo/Maven/demo/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Johanna Fernando!";
+        return "Hello Spring Boot!";
     }
 }
